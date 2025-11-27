ea9bfe6e8792ac51dd22429822c058c9ccb76ccd
Shandler Mason <samason4@ncsu.edu>
2025-11-27 00:36:54 -0500

    another picture change

--- shandler-mason-portfolio/src/sections/profile.tsx
+++ shandler-mason-portfolio/src/sections/profile.tsx
@@ -18,8 +18,8 @@ export default function Profile() {
                 style={{ 
                   width: '350px',  // Set width
                   height: '350px', // Set height equal to width to keep it square
-                  border: '10px solid #005A8D',
-                  borderRadius: '15%',  // Apply rounded corners
+                  border: '7px solid #005A8D',
+                  //borderRadius: '15%',  // Apply rounded corners
                   margin: '.25rem'
                 }}
               />
